d012524e8eb801b0accae7001f32cae81aac9616
Manya Vishwakarma <M8077anya-Vishwakarma@users.noreply.huggingface.co>
2025-12-19 06:11:21 +0000

    created code in init

---
+++ agents/__init__.py
@@ -0,0 +1,15 @@
+"""
+Agents package for Salesforce Security Analyzer
+Contains core logic for API interactions and analysis
+"""
+
+from .salesforce_client import SalesforceClient
+from .codegen_analyzer import CodeGenSecurityAnalyzer, HybridAnalyzer
+
+__all__ = [
+    'SalesforceClient',
+    'CodeGenSecurityAnalyzer',
+    'HybridAnalyzer'
+]
+
+__version__ = '1.0.0'
